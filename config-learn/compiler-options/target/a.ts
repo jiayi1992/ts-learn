ab437444ec130515871fb89be7c787fed06adc16
Jiayi Xin <jiayi920429@gmail.com>
2023-04-30 20:15:32 -0700

    add lib

--- config-learn/compiler-options/target/a.ts
+++ config-learn/compiler-options/target/a.ts
@@ -1,1 +1,2 @@
+// For example, an arrow function () => this will be turned into an equivalent function expression if target is ES5 or lower.
 export const print = () => console.log("This is a.ts");
