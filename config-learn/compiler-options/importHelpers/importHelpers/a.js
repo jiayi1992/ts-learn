39aa962dfecc4d4d14132f67e1571e2cb73dbc4a
Jiayi Xin <jiayi920429@gmail.com>
2023-05-02 18:24:49 -0700

    add importHelpers

---
+++ config-learn/compiler-options/importHelpers/importHelpers/a.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.fn = void 0;
+var tslib_1 = require("tslib");
+function fn(arr) {
+    var arr2 = tslib_1.__spreadArray([1], arr, true);
+}
+exports.fn = fn;
---
+++ config-learn/compiler-options/importHelpers/noImportHelpers/a.js
@@ -0,0 +1,16 @@
+"use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.fn = void 0;
+function fn(arr) {
+    var arr2 = __spreadArray([1], arr, true);
+}
+exports.fn = fn;
