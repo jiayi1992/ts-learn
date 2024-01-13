a91dfdd7489745c8576d3221969071d1e3f1ff85
Jiayi Xin <jiayi920429@gmail.com>
2024-01-12 17:40:02 -0800

    refactor lib and baseUrl

--- config-learn/compiler-options/baseUrl/a.js
+++ config-learn/compiler-options/baseUrl/a.js
@@ -1,9 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.print = void 0;
-var b_1 = require("./sub/b");
-var print = function () {
-  return console.log("This is a.ts");
-};
+var b_1 = require("sub/b");
+var print = function () { return console.log("This is a.ts"); };
 exports.print = print;
 (0, b_1.print)();
---
+++ config-learn/compiler-options/lib/es6/a.ts
@@ -0,0 +1,15 @@
+// TypeScript only transpiles new JavaScript syntax like arrow functions and async functions,
+// but it does not provide polyfills for JavaScript APIs, like Promise, array.find, Map, etc. (unlike Babel).
+
+// Therefore, it is up to you to include polyfills for JavaScript APIs that your
+// runtime environment might not support. So, if you want to use promises in an ES5
+// environment, you would include a Promise polyfill.
+
+// However, you will run into a problem. Even though you're importing a Promise polyfill,
+// TypeScript does not know that, and shows the error we saw before.
+// That's why the lib option was introduced. lib tells TypeScript which library APIs
+// will be available in your runtime environment.
+export const promise = new Promise(() => {});
+
+const window = null;
+const test = window?.attribute;
